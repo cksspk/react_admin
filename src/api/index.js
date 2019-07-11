0dc1b0273f7736209c29590d7f9c966efe2fafec
cksspk <cksspk@163.com>
2019-07-12 00:52:56 +0800

    动首页头部信息，时间，天气

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,9 @@
 
 //引入自己写的ajax模块
 import ajax from './ajax'
+import jsonp from 'jsonp'
+
+import {message} from 'antd'
 
 //登录
 const BASE = '';
@@ -17,3 +20,26 @@ export const reqLogin = (username, password) => ajax(BASE+'/login',{username, pa
 
 //添加用户
 export const reqAddUser = (user) => ajax(BASE+'/manage/user/add',user,'POST');
+
+/**
+ jsonp请求的接口函数
+ */
+export const reqWeather = (city) => {
+    return new Promise((resolve, reject) => {
+        const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+        // 发送jsonp请求
+        jsonp(url, {}, (err, data) => {
+            console.log('jsonp()', err, data)
+            // 如果成功了
+            if (!err && data.status==='success') {
+                // 取出需要的数据
+                const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+                resolve({dayPictureUrl, weather})
+            } else {
+                // 如果失败了
+                message.error('获取天气信息失败!')
+            }
+
+        })
+    })
+}
--- src/components/swieepage/index.js
+++ src/components/swieepage/index.js
@@ -1,6 +1,6 @@
-// import React,{Component} from 'react'
-// import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-//
+import React,{Component} from 'react'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
 // //引入Redirect标签，重定向 //引入路由组件
 // import {Redirect, Route, Switch} from 'react-router-dom'
 //
@@ -17,75 +17,15 @@
 //
 // const { SubMenu } = Menu;
 // const { Header, Content, Sider } = Layout;
-//
-// export default class Index extends  Component {
-//     /*设置默认值*/
-//     constructor(props) {
-//         super(prps);
-//         this.state = {
-//             visible: false,
-//             itemData: {}
-//         };
-//     }
-//         addAction = () =>{
-//             this.setState( {
-//                 visible: true,
-//                 itemData
-//             });
-//         };
-//
-//     /*获取子组件传递过来的值*/
-//
-//     changeStatus = (status) =>{
-//         this.setState({
-//             visible:status
-//         })
-//     }
-//
-//         render()
-//         {
-//             return (
-//                 <Layout style={{height: '100%', backgroundColor: 'gray'}}>
-//                     <div style={{textAlign: 'center'}}><h1 style={{fontSize: '20px', fontWeight: "bold"}}>电子对抗行动控制</h1>
-//                     </div>
-//                     <Header style={{padding: '0'}}>
-//                         <Menu
-//                             mode="horizontal"
-//                             defaultSelectedKeys={1}
-//                             style={{lineHeight: '64px'}}
-//                         >
-//                             <Menu.Item key="1">nav 1</Menu.Item>
-//                             <Menu.Item key="2">nav 2</Menu.Item>
-//                             <Menu.Item key="3">nav 3</Menu.Item>
-//                             <Button onClick={this.addAction.bind(this)}>添加</Button>
-//                             <AddAction
-//                                        visible={visible}
-//                                        entray={itemData}
-//                                        status={this.changeStatus}
-//                             />
-//                         </Menu>
-//                     </Header>
-//                     <div style={{height: '50px', textAlign: 'center'}}>滚动条</div>
-//                     <Layout>
-//                         <Sider width={200} style={{background: 'white'}}>
-//                             <LeftSider/>
-//                         </Sider>
-//                         <Content style={{background: '#fff', padding: 24, margin: 0, minHeight: 280,}}>
-//                             <Switch>
-//                                 {/*子路由配置需要前缀加上父路由*/}
-//                                 <Route path='/swiee/page1' component={Page1}/>
-//                                 <Route path='/swiee/page2' component={Page2}/>
-//                                 <Route path='/swiee/page3' component={Page3}/>
-//                                 <Route path='/swiee/page4' component={Page4}/>
-//                                 <Route path='/swiee/page5' component={Page5}/>
-//                                 <Route path="/swiee/page6" component={Page6}/>
-//                                 <Route/>
-//                             </Switch>
-//                         </Content>
-//
-//                     </Layout>
-//                 </Layout>
-//             )
-//         }
-//
-// }
+
+export default class Index extends  Component {
+
+
+        render()
+        {
+            return (
+               <div>test</div>
+            )
+        }
+
+}
